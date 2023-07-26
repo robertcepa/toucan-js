66f08ca5f2774f979ca167f06532757555cf78c4
Robert Cepa <robertcepa@icloud.com>
2023-07-26 02:30:49 -0700

    Re-export integrations (#192)
    * Re-export integrations
    
    * Create thick-badgers-prove.md
    
    * Fix build

--- packages/toucan-js/src/index.ts
+++ packages/toucan-js/src/index.ts
@@ -1,4 +1,12 @@
-export { LinkedErrors, RequestData } from './integrations';
+export {
+  LinkedErrors,
+  RequestData,
+  Dedupe,
+  ExtraErrorData,
+  RewriteFrames,
+  SessionTiming,
+  Transaction,
+} from './integrations';
 export type { LinkedErrorsOptions, RequestDataOptions } from './integrations';
 export { Toucan } from './sdk';
 export type { Options } from './types';
