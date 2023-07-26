80deaaae6f5dd58f01b5a0b1e111d432275aa7b5
Robert Cepa <robertcepa@icloud.com>
2023-07-26 03:47:54 -0700

    Add fetcher option to transportOptions

--- packages/toucan-js/src/transports/types.ts
+++ packages/toucan-js/src/transports/types.ts
@@ -2,7 +2,18 @@ import type { BaseTransportOptions } from '@sentry/types';
 import type { Context } from '../types';
 
 export type FetchTransportOptions = BaseTransportOptions & {
+  /**
+   * Custom headers passed to fetch.
+   */
   headers?: Record<string, string>;
 
+  /**
+   * Cloudflare Workers context.
+   */
   context?: Context;
+
+  /**
+   * Custom fetch function.
+   */
+  fetcher?: typeof fetch;
 };
